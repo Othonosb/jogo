27b536bed4c759a353ca66dc16091c9f3fcf3dde
othonosb <othonosb@hotmail.com>
2022-01-11 02:56:35 -0300

    adicionando index.html personagem jack

---
+++ jack.js
@@ -0,0 +1,36 @@
+
+
+
+function additem() {
+    alert("OLÁ! ESTOU INVESTIGANDO O ROUBO AO BANCO. PRECISO FAZER ALGUMAS PERGUNTAS E SE MENTIR VAI PRESO!");
+    var acaoJack = prompt("VOCÊ CONHECE A PAYTON? SIM OU NÃO");
+    if(acaoJack === "conheço" || acaoJack === "sim" || acaoJack === "SIM" || acaoJack === "Sim" || acaoJack === "CONHEÇO"){
+        confirm("ENTÃO MINHAS SUSPEITAS ESTÃO CORRETAS...");
+    
+}
+if(acaoJack === "nao" || acaoJack === "não" || acaoJack === "NÃO" || acaoJack === "Não" || acaoJack === "Não conheço"){
+    var pergunta3 = confirm("GAME OVER! VOCÊ NÃO PODE MENTIR PARA MIM! A VERDADE É SEMPRE MELHOR...");
+        if(pergunta3 === true){
+            location = "infojack.html"
+            return;
+}
+}
+
+    alert("VAMOS PRA MAIS OUTRA...");
+    var acaoJack2 = prompt("VOCÊ A VIU ONTEM A NOITE?");
+    if(acaoJack2 === "sim" || acaoJack2 === "vi" || acaoJack2 === "vi sim" || acaoJack2 === "SIM" || acaoJack2 === "Vi Sim" || acaoJack2 === "eu vi"){
+       alert("HMMM.. HORA DE ME CONTA TUDO VOU VAI VIRAR POEIRA!");
+        if(pergunta3 === true){
+            location = "jackfase2.html";
+    }
+    }
+
+    if(acaoJack2 === "nao" || acaoJack2 === "não" || acaoJack2 == "Não" || acaoJack2 == "NÃO" || acaoJack2 == "NÃO VI" || acaoJack2 == "Nao vi"){
+        var pergunta2 = confirm("GAME OVER! VOCÊ ESTÁ MENTINDO! SEGUIMOS TODOS SEUS PASSOS!");
+        if(pergunta2 === true){
+            location = "infojack.html"
+            return;
+    }
+    }
+}
+     
