7f7d3ce767814deb3396ae887bb26a0af795734f
othonosb <othonosb@hotmail.com>
2022-01-12 21:46:57 -0300

    ultimas implementacao html e css

--- paytonBanco.js
+++ paytonBanco.js
@@ -1,15 +1,15 @@
 function decidir(){
     alert("QUAL A MELHOR FORMA DE EU ME DAR BEM HOJE?");
     var acaoPayton = prompt("ATACAR AGORA OU ESPERAR ANOITECER");
-    if(acaoPayton === "atacar agora" ||  acaoPayton === "ATACAR AGORA" || acaoPayton === "Atacar agora" || acaoPayton === "Atacar Agora"){
+    if(acaoPayton === "atacar" || acaoPayton === "ATACAR" || acaoPayton === "atacar agora" ||  acaoPayton === "ATACAR AGORA" || acaoPayton === "Atacar agora" || acaoPayton === "Atacar Agora"){
     var confirmFase2 = confirm("GAME OVER! VOCÊ NÃO VAI CONSEGUIR ASSALTAR COM TANTOS SEGURANÇAS NA ENTRADA! ATACAR AGORA NAO É UMA OPÇAO INTELIGENTE!");
         if(confirmFase2 === true){
-            location = "index.html";
+            location = "infopayton.html";
             return;
         }
     }
 
-    if(acaoPayton === "esperar anoitecer" || acaoPayton == "ESPERAR ANOITECER" || acaoPayton === "Esperar anoitecer" || acaoPayton === "Esperar Anoitecer"){
+    if(acaoPayton === "esperar" || acaoPayton === "ESPERAR" || acaoPayton === "esperar anoitecer" || acaoPayton == "ESPERAR ANOITECER" || acaoPayton === "Esperar anoitecer" || acaoPayton === "Esperar Anoitecer"){
         alert("ISSO! A NOITE NÃO VAI TER NINGUEM, VAI SER A HORA PERFEITA PRA ROUBAR!");
        var gameOver = confirm("NÃO FALEI?! SOU RÁPIDA DEMAIS! E ESSA FOI COMO TIRAR DOCE DE CRIANÇA! PŔOXIMA!");
         if(gameOver === true){
